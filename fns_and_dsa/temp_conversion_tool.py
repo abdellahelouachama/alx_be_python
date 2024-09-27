29e5857c10850a71fb9f8a549929526a22fd87e0
abdellah el ouachama <abdellahelouchaa@gmail.com>
2024-09-27 15:36:38 +0100

    0

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,8 +2,10 @@ CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9 
 
 def convert_to_celsius(fahrenheit):
+    global choise
+    choise = 'C'
     celsius = FAHRENHEIT_TO_CELSIUS_FACTOR * (fahrenheit - 32)
-    print(f"{round(fahrenheit, 2)}°F is {float(celsius)}°C")
+    print(f"{round(fahrenheit, 2)}°F is {float(celsius)}°{choise}")
 
 
 
@@ -11,15 +13,16 @@ def convert_to_celsius(fahrenheit):
 
 
 def onvert_to_fahrenheit(celsius):
+    global choise
+    choise = 'F'
     fahrenheit = (CELSIUS_TO_FAHRENHEIT_FACTOR * celsius) + 32
-    print(f"{round(celsius, 2)}°C is {float(fahrenheit)}°F")
+    print(f"{round(celsius, 2)}°C is {float(fahrenheit)}°{choise}")
 
 
 
 
 temperature = int(input("Enter the temperature to convert: "))
 if type(temperature) == int:
-    print("nvalid temperature. Please enter a numeric value.")
 
     choise = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").upper().strip()
 
