392ce5178dada0afcc7af65b7abbbfb1d4176330
abdellah el ouachama <abdellahelouchaa@gmail.com>
2024-09-27 11:36:10 +0100

    small changes

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,5 +9,5 @@ def perform_operation(num1, num2, operation):
         if num2 != 0:
             return float(num1 / num2)
         else:
-            return float(num2)
+            return "Cannot divide by zero."
                       
