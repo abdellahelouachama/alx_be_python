b25a0ae80da6e5ba6631e7ba505f2d0b0f059ba1
abdellah el ouachama <abdellahelouchaa@gmail.com>
2024-09-27 12:02:14 +0100

    created a script to manges shopping list

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,42 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            # Prompt for and add an item
+            item = input("Entre an item to add it to your shopping list: ")
+            shopping_list.append(item)
+            pass
+
+        elif choice == '2':
+            # Prompt for and remove an item
+            removed_item = input("Entre the item you want to remove: ").lower().strip()
+            if removed_item in shopping_list:
+                shopping_list.remove(removed_item)
+            else:
+                print(f"this item {removed_item} is not found in your shopping list")    
+            pass
+
+        elif choice == '3':
+            # Display the shopping list
+            for item in shopping_list:
+                print(item)
+            pass
+        
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
