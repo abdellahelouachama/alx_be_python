33acd2dfdec2f8305f869fff67fa8f2d915f0a2d
abdellah el ouachama <abdellahelouchaa@gmail.com>
2024-10-04 11:49:15 +0100

    new change

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -13,7 +13,7 @@ class BankAccount:
             return False
 
     def display_balance(self):
-        print(f"Current Balance: ${round(self.account_balance, 2)}")
+        print(f"Current Balance: ${self.account_balance}.00")
 
 
         
