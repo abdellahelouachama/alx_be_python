a1b9bdb59d7d713ee9e13c71af35a2c6559d07a8
abdellah el ouachama <abdellahelouchaa@gmail.com>
2024-10-05 11:54:48 +0100

    created new script

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,26 @@
+class Book:
+    def __init__(self, title, author, _is_checked_out  = False):
+        self.title = title
+        self.author = author
+        self._is_checked_out = _is_checked_out
+
+class Library:   
+    _books = []
+    # _is_checked_out = None
+
+    def add_book(cls, book):
+        cls._books.append(book)
+    def check_out_book(cls, title):
+        for book in cls._books:
+            if book.title == title:
+                book._is_checked_out = True
+
+    def return_book(cls, title):    
+        for book in cls._books:
+            if book == title:
+                book._is_checked_out = False
+
+    def list_available_books(cls):
+        for book in cls._books:
+            if not book._is_checked_out:
+                print(f"{book.title} by {book.author}")
