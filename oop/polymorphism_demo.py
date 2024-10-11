8b728489f2a0b70a4d6a70e909fd6852822ab252
abdellah el ouachama <abdellahelouchaa@gmail.com>
2024-10-11 12:26:52 +0100

    crearted a scrpit that calclate the area of shapes

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -1,8 +1,14 @@
 import math
 class Shape:
     def area(self):
-        raise NotImplementedError
-    
+        raise NotImplementedError    
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+    def area(self):
+        area = self.length * self.width
+        return area    
 class Circle(Shape):
     def __init__(self, radius):
         self.radius = radius
@@ -11,11 +17,4 @@ class Circle(Shape):
         return area
         
 
-class Rectangle(Shape):
-    def __init__(self, length, width):
-        self.length = length
-        self.width = width
-    def area(self):
-        area = self.length * self.width
-        return area    
 
